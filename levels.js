bce30a90a07ec1a7a9d62cc057fd7ad4cab25f53
hanna162 <farhana1602@gmail.com>
2026-07-20 14:21:31 +0800

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,72 @@
+const levels = [
+
+    {
+        id:1,
+        name:"Number Forest 🌳",
+        unlocked:true
+    },
+
+
+    {
+        id:2,
+        name:"Addition Valley",
+        unlocked:false
+    },
+
+
+    {
+        id:3,
+        name:"Subtraction Cave",
+        unlocked:false
+    },
+
+
+    {
+        id:4,
+        name:"Multiplication Mountain",
+        unlocked:false
+    },
+
+
+    {
+        id:5,
+        name:"Division Desert",
+        unlocked:false
+    },
+
+
+    {
+        id:6,
+        name:"Math Castle",
+        unlocked:false
+    },
+
+
+    {
+        id:7,
+        name:"Logic World",
+        unlocked:false
+    },
+
+
+    {
+        id:8,
+        name:"Challenge Island",
+        unlocked:false
+    },
+
+
+    {
+        id:9,
+        name:"Brain Tower",
+        unlocked:false
+    },
+
+
+    {
+        id:10,
+        name:"Math Champion",
+        unlocked:false
+    }
+
+];
